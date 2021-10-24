d909396c8e237cec20270689fff74069c3e74537
Vishwak-Kiran <vishwakkiran2002@gmail.com>
2021-10-24 13:57:47 +0530

    Co-authored-by: Gokul eswaran <GokZ465@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:learn/screens/home/home.dart';
+import 'package:learn/auth/auth_screen.dart';
+import 'package:learn/auth/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+//import 'package:flutter_auth_animation/auth_screen.dart';
+//import 'package:firebase_core/firebase_core.dart';
+
 // ignore: duplicate_import
 
 void main() {
@@ -14,9 +18,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
         const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white38,
+          border: InputBorder.none,
+          hintStyle: TextStyle(color: Colors.white),
+          contentPadding: EdgeInsets.symmetric(
+              vertical: defaultPadding * 1.2, horizontal: defaultPadding),
+        ),
+      ),
+      home: AuthScreen(),
     );
   }
 }
